f837c0e5b64b07c3b934357512ea2cc10ec9dd22
jackbolton901 <bltnjck@gmail.com>
2019-08-26 16:39:08 -0500

    afternoon classwork. functions

---
+++ functions.py
@@ -0,0 +1,39 @@
+# problem1
+# create function
+def printNumbers():
+    for x in range(-25, 21, 1):
+        print(x)
+# calling function
+printNumbers()
+
+# problem2
+
+
+var1 = input("Enter Password  ")
+var2 = input("Confirm Password  ")
+def checkPassword(var1,var2):
+    if var1 == var2:
+        return "true"
+    else:
+        return "false"
+print(checkPassword(var1, var2))
+
+# Problem3
+
+def evenOrOdd(num):
+    
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+        
+num = int(input("enter a number.  "))
+evenOrOdd(num)
+print(evenOrOdd(num))
+
+# Problem4
+
+def problem4():
+    greet("hello")
+
+def greet():
