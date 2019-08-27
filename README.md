cf9ddbb409ab65f6deb872009cc03b125a9e2f30
autumn-ragland <raglanda998@gmail.com>
2019-08-27 09:42:09 -0500

    Graded 08-27-2019 09:40:50

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Python review function introduction
-
+3/5. Score : 3
 ### Problem 1
 Create a ```printNumbers``` function to print integers from -25 to 20 to the console (print in the function)
 
--- functions.py
+++ functions.py
@@ -7,8 +7,6 @@ def printNumbers():
 printNumbers()
 
 # problem2
-
-
 var1 = input("Enter Password  ")
 var2 = input("Confirm Password  ")
 def checkPassword(var1,var2):
@@ -18,8 +16,7 @@ def checkPassword(var1,var2):
         return "false"
 print(checkPassword(var1, var2))
 
-# Problem3
-
+# problem3
 def evenOrOdd(num):
     
     if num % 2 == 0:
@@ -28,12 +25,11 @@ def evenOrOdd(num):
         return "odd"
         
 num = int(input("enter a number.  "))
-evenOrOdd(num)
+# evenOrOdd(num)
 print(evenOrOdd(num))
 
 # Problem4
-
 def problem4():
     greet("hello")
 
-def greet():
+# def greet():
